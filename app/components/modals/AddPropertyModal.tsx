4521f5fb27d1de3a2e028e63085ff128965fe925
LOFRZ <almenzo.arroyo@gmail.com>
2024-09-18 01:40:50 +0200

    text area

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -14,8 +14,10 @@ const AddPropertyModal = () => {
     //
     // state
     
-    const [CurrentStep, setCurrentStep] = useState(1);
+    const [currentStep, setCurrentStep] = useState(1);
     const [dataCategory, setDataCategory] = useState('');
+    const [dataTitle, setDataTitle] = useState('');
+    const [dataDescription, setDataDescription] = useState('');
 
     //
     //
@@ -34,7 +36,7 @@ const AddPropertyModal = () => {
 
     const content = (
         <>
-            {CurrentStep == 1 ? (
+            {currentStep == 1 ? (
                 <>
                     <h2 className="mb-6 text-2xl">Choose category</h2>
 
@@ -48,8 +50,44 @@ const AddPropertyModal = () => {
                         onClick={() => setCurrentStep(2)}
                     />
                 </>
+            ) : currentStep == 2 ? (
+                <>
+                    <h2 className="mb-6 text-2xl">Describe your place</h2>
+
+                    <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Title</label>
+                            <input
+                                type="text"
+                                value={dataTitle}
+                                onChange={(e) => setDataTitle(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Description</label>
+                            <textarea
+                                value={dataDescription}
+                                onChange={(e) => setDataDescription(e.target.value)}
+                                className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <CustomButton
+                        label="Previous"
+                        className="mb-2 bg-black hover:bg-gray-800"
+                        onClick={() => setCurrentStep(1)}
+                    />
+
+                    <CustomButton
+                        label="Next"
+                        onClick={() => setCurrentStep(3)}
+                    />
+                </>
             ) : (
-                <p>step 2</p>
+                <p>szghh</p>
             )}
         </>
     )
