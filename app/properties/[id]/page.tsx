0011d86d2a4cef9df8ee7f77b5de5d904e9380f1
LOFRZ <almenzo.arroyo@gmail.com>
2024-09-27 11:04:40 +0200

    ppp

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -21,35 +21,39 @@ const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
 
         <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
             <div className="py-6 pr-6 col-span-3">
-                <h1 className="mb-4 text-4xl">Prop Name</h1>
+                <h1 className="mb-4 text-4xl">{property.title}</h1>
 
                 <span className="mb-6 block text-lg text-gray-600">
-                    GuestS
+                    {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
                 </span>
 
 
                 <hr />
 
                 <div className="py-6 flex items-center space-x-4">
-                    <Image
-                        src="/profile_pic_1.jpg"
-                        width={50}
-                        height={50}
-                        className="rounded-full"
-                        alt="User name"
-                    />
+                    {property.landlord.avatar_url && (
+                        <Image
+                            src={property.landlord.avatar_url}
+                            width={50}
+                            height={50}
+                            className="rounded-full"
+                            alt="User name"
+                        />
+                    )}
 
-                    <p><strong>Jhon</strong> is your hote</p>
+                    <p><strong>{property.landlord.name}</strong> is your hote</p>
                 </div>
 
                 <hr />
 
                 <p className="mt-6 text-lg">
-                    Lorem fzsozuhgeiluqùpaieghQMZYGzmogulizR
+                    {property.description}
                 </p>
             </div>
 
-            <ReservationSidebar />
+            <ReservationSidebar 
+                property={property}
+            />
         </div>
     </main>
   )
