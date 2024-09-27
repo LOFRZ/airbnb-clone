ace195205dae236c6a9ead377a2d8f04c31e7aad
LOFRZ <almenzo.arroyo@gmail.com>
2024-09-27 10:45:30 +0200

    ttttt

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,7 +1,13 @@
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar"
 import Image from "next/image"
 
-const PropertyDetailPage = () => {
+import apiService from "@/app/services/apiSevice";
+
+
+const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`)
+
+
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6"> 
         <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
