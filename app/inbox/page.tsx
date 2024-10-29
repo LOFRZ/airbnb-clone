7186206a7c2b9d7f12b2905782514b8eb53c4ea2
LOFRZ <almenzo.arroyo@gmail.com>
2024-10-29 01:59:58 +0100

    t

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -31,9 +31,9 @@ const InboxPage = async () => {
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
         <h1 className="my-6 text-2xl">Inbox</h1>
 
-        {Conversation.map((Conversation: ConversationType) => {
+        {conversations.map((conversation: ConversationType) => {
           return (
-            <Conversation />
+              <Conversation />
           )
         })}
     </main>
